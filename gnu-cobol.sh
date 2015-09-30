edd956a86c33d959d880f84345c9b9fbe0a09d1c
Quinn Evans <heddwch@lambdaos.org>
2015-09-30 12:50:18 -0600

    gnu-cobol.sh: Add flag handling, -d for debug build.

--- gnu-cobol.sh
+++ gnu-cobol.sh
@@ -1,5 +1,23 @@
 #!/bin/sh
-${CC:-cc} -o channel.o -c channel.c
-${COBC:-cobc} -x WOPO-CNF.COB PRINTCNF.COB
-${COBC:-cobc} -x WOPO.COB IRC-MSG.COB PRINTCNF.COB DECASCII.COB ENCASCII.COB BF-RUN.COB channel.o
+args=`getopt d $*`
+if [ $? -ne 0 ]; then
+    echo 'Usage: sh gnu-cobol.sh [-d]'
+    exit 2
+fi
+set -- $args
+while true; do
+    case "$1" in
+    -d)
+        DEBUG=1
+        shift
+        ;;
+    --)
+        shift
+        break
+        ;;
+    esac
+done
+${CC:-cc} ${DEBUG:+-DDEBUG} -o channel.o -c channel.c
+${COBC:-cobc} ${DEBUG:+-fdebugging-line} -x WOPO-CNF.COB PRINTCNF.COB
+${COBC:-cobc} ${DEBUG:+-fdebugging-line} -x WOPO.COB IRC-MSG.COB PRINTCNF.COB DECASCII.COB ENCASCII.COB BF-RUN.COB channel.o
 
