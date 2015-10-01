c40b9a35090e76f8b6c47f7d2140e39ea3c831fe
Quinn Evans <heddwch@lambdaos.org>
2015-10-01 14:49:57 -0600

    Converted channel.c to use ASCII in cobol numerals on COBOL side.

--- channel.c
+++ channel.c
@@ -9,119 +9,102 @@
 #include <string.h>
 #include <unistd.h>
 
-#define LENGTH_DIGITS 3
-#define DEFAULT_PORT "6667"
-#define RECV_BUF_SIZE 1024
-/*
- * 01 BUFFER
- *     03 MSG-LENGTH PIC 9(LENGTH_DIGITS)
- *     03 MSG-BODY PIC X(x)
- */
-#define BODY(buf) (char *)(buf + LENGTH_DIGITS)
 
-// 01 CHANNEL-STATUS PIC 9(2)
+#define CELL_DIGITS 3
+#define BUFFER_SIZE 512
+/*
+ * 01 COBOL-BUFFER PIC 9(CELL_DIGITS) OCCURS BUFFER_SIZE TIMES
+ */
+char msg_buf[BUFFER_SIZE];
+
+#define STATE_DIGITS 2
+// 01 CHANNEL-STATUS PIC 9(STATE_DIGITS)
 #define EBADDEST 10
 #define EOPENFAIL 20
 #define EHUP 30
 #define ESERV 40
 
-char *msg_length, *msg_body, *channel_status;
-int msg_body_len;
+#define DEFAULT_PORT "6667"
 int sockfd;
+
+#define RECV_BUF_SIZE 1024
 char recv_buf[RECV_BUF_SIZE];
 size_t recv_buf_pos = 0;
 
-int channel_message_length(void)
+void channel_set_status(char *state, int value)
 {
-  char c_length[LENGTH_DIGITS + 1];
-  memcpy(c_length, msg_length, LENGTH_DIGITS);
-  c_length[LENGTH_DIGITS] = '\0';
-  return (int)strtol(c_length, NULL, 10) - 1;
-}
-
-void channel_set_status(int value)
-{
-  char buf[9];
-  sprintf(buf, "%08u", value);
-  memcpy(channel_status, buf + 6, 2);
+  char buf[STATE_DIGITS + 1];
+  snprintf(buf, STATE_DIGITS + 1, "%.*hhu", STATE_DIGITS, (unsigned char)value);
+  memcpy(state, buf, STATE_DIGITS);
 
   return;
 }
-void channel_from_cobol(void)
+
+void channel_from_cobol(char *cobol_buffer)
 {
-  int message_length = channel_message_length();
-  if (message_length == msg_body_len + 1) {
-    int i;
-    for(i = 0; i < msg_body_len; i++) {
-      if(msg_body[i] != ' ') {
+  char buf[CELL_DIGITS + 1];
+  buf[CELL_DIGITS] = '\0';
+  memcpy(buf, cobol_buffer, CELL_DIGITS);
+
+  int i;
+  for(i = 0; strcmp(buf, "000") && i < BUFFER_SIZE; memcpy(buf, cobol_buffer + ++i * CELL_DIGITS, CELL_DIGITS)) {
+    msg_buf[i] = (char)strtol(buf, NULL, 10);
+  }
+  msg_buf[i] = '\0';
+  if (i == BUFFER_SIZE) {
+    int message_length = 0;
+    for(i = 0; i < BUFFER_SIZE; i++) {
+      if(msg_buf[i] != ' ') {
 	message_length = i + 1;
       }
     }
-  }
-  memset(msg_body + message_length, 0, msg_body_len - message_length + 1);
-  for(char *c = msg_body; *c; c++) {
-    if(*c == ';') *c = ':';
+    if(message_length == BUFFER_SIZE) {
+      message_length--;
+    }
+    msg_buf[message_length] = '\0';
   }
   return;
 }
 
-void channel_to_cobol(void)
+void channel_to_cobol(char *cobol_buffer)
 {
-  char *c;
-  for(c = msg_body; *c; c++) {
-    if(isalpha(*c)) *c = toupper(*c);
-    if(*c == ':') *c = ';';
+  char buf[CELL_DIGITS + 1];
+  for(int i = 0; i < BUFFER_SIZE && msg_buf[i]; i++) {
+    snprintf(buf, CELL_DIGITS + 1, "%.*hhu", CELL_DIGITS, msg_buf[i]);
+    memcpy(cobol_buffer + i * CELL_DIGITS, buf, CELL_DIGITS);
   }
-  memset(c, ' ', msg_body_len - strlen(msg_body));
+  memset(cobol_buffer + (BUFFER_SIZE - 1) * CELL_DIGITS, (int)'0', CELL_DIGITS);
 
   return;
 }
 
-void channel_string_to_cobol(const char *s)
+void channel_string_to_cobol(char *cobol_buffer, const char *s)
 {
-  strncpy(msg_body, s, msg_body_len - 2);
-  msg_body[msg_body_len - 1] = '\0';
-  channel_to_cobol();
+  strncpy(msg_buf, s, BUFFER_SIZE);
+  msg_buf[BUFFER_SIZE - 1] = '\0';
+  channel_to_cobol(cobol_buffer);
   return;
 }
 
 /*
- *         MOVE LENGTH OF MSG-BODY OF BUFFER TO MSG-LENGTH OF BUFFER.
- *         CALL "CHANNEL-INIT" USING BUFFER, STATE.
+ *         ASCII "HOST[:PORT]$NUL$" IN COBOL-BUFFER
+ *         CALL "CHANNEL-OPEN" USING COBOL-BUFFER, STATE.
  */
-void CHANNEL__INIT(char *buffer, char *state)
+void CHANNEL__OPEN(char *cobol_buffer, char *state)
 {
-  msg_length = buffer;
-  msg_body = BODY(buffer);
-  msg_body_len = channel_message_length();
-  channel_status = state;
-
-  return;
-}
-
-/*
- *         STRING "HOST[:PORT]"
- *                INTO MSG-BODY
- *                WITH POINTER MSG-LENGTH.
- *         CALL "CHANNEL-OPEN".
- *
- * Sets channel_status
- */
-void CHANNEL__OPEN(void)
-{
-  channel_from_cobol();
-  if(!strlen(msg_body)) {
-    channel_string_to_cobol("No host specified");
-    channel_set_status(EBADDEST);
+  channel_from_cobol(cobol_buffer);
+  if(!strlen(msg_buf)) {
+    channel_string_to_cobol(cobol_buffer, "No host specified");
+    channel_set_status(state, EBADDEST);
     return;
  }
-  char *port = strchr(msg_body, ':');
+  char *port = strchr(msg_buf, ':');
   if(port) {
     *port = '\0';
     port++;
     if(!strlen(port)) {
-      channel_string_to_cobol("Port separator specified, but not port");
-      channel_set_status(EBADDEST);
+      channel_string_to_cobol(cobol_buffer, "Port separator specified, but not port");
+      channel_set_status(state, EBADDEST);
       return;
     }
   } else {
@@ -134,9 +117,9 @@ void CHANNEL__OPEN(void)
   hints.ai_family = AF_UNSPEC;
   hints.ai_socktype = SOCK_STREAM;
   hints.ai_flags = AI_ADDRCONFIG;
-  if((status = getaddrinfo(msg_body, port, &hints, &res))) {
-    channel_string_to_cobol(gai_strerror(status));
-    channel_set_status(EBADDEST);
+  if((status = getaddrinfo(msg_buf, port, &hints, &res))) {
+    channel_string_to_cobol(cobol_buffer, gai_strerror(status));
+    channel_set_status(state, EBADDEST);
     return;
   }
 
@@ -157,68 +140,67 @@ void CHANNEL__OPEN(void)
   }
   
   if(!curr_addr) {
-    channel_string_to_cobol("Unable to connect to host");
-    channel_set_status(EOPENFAIL);
+    channel_string_to_cobol(cobol_buffer, "Unable to connect to host");
+    channel_set_status(state, EOPENFAIL);
     return;
   }
 
-  channel_set_status(0);
+  channel_set_status(state, 0);
   return;
 }
 
-// Sets channel_status
-void CHANNEL__SEND(void)
+//         CALL "CHANNEL-SEND" USING COBOL-BUFFER, STATE.
+void CHANNEL__SEND(char *cobol_buffer, char *state)
 {
   char *msg;
   int sent, total;
-  channel_from_cobol();
+  channel_from_cobol(cobol_buffer);
 #ifdef DEBUG
-  printf("Sending: %s\n", msg_body);
+  printf("Sending: %s\n", msg_buf);
 #endif
   sent = 0;
-  total = strlen(msg_body);
-  msg_body[total++] = '\n';
+  total = strlen(msg_buf);
   while(sent < total) {
-    int status = send(sockfd, msg_body + sent, total - sent, 0);
-    if(status == -1) {
+    int status = send(sockfd, msg_buf + sent, total - sent, 0);
+ if(status == -1) {
       perror("send");
       close(sockfd);
-      channel_string_to_cobol("Hung up");
-      channel_set_status(EHUP);
+      channel_string_to_cobol(cobol_buffer, "Hung up");
+      channel_set_status(state, EHUP);
       return;
     }
     sent += status;
   }
 
-  channel_set_status(0);
+  channel_set_status(state, 0);
   return;
 }
 
-// Sets channel_status
-void CHANNEL__RECV(void)
+//         CALL "CHANNEL-RECV" USING COBOL-BUFFER, STATE.
+void CHANNEL__RECV(char *cobol_buffer, char *state)
 {
   char *message_end;
  get_buffered:
   message_end = memchr(recv_buf, '\n', recv_buf_pos);
   if(message_end) {
     size_t message_size = message_end - recv_buf;
-    if (message_size > msg_body_len) {
-      channel_string_to_cobol("Server sent too-long message");
-      channel_set_status(ESERV);
+    if (message_size > BUFFER_SIZE) {
+      channel_string_to_cobol(cobol_buffer, "Server sent too-long message");
+      channel_set_status(state, ESERV);
       return;
     }
-    memcpy(msg_body, recv_buf, message_size);
-    msg_body[message_size - 1] = '\0';
+    memcpy(msg_buf, recv_buf, message_size);
+    msg_buf[message_size] = '\0';
     recv_buf_pos -= message_size + 1;
     message_end++;
     for(size_t i = 0; i < recv_buf_pos; i++) {
       recv_buf[i] = message_end[i];
     }
 #ifdef DEBUG
-    printf("Received: %s\n", msg_body);
+    printf("Received: %s\n", msg_buf);
 #endif
-    channel_to_cobol();
-    channel_set_status(0);
+    channel_to_cobol(cobol_buffer);
+    channel_set_status(state, 0);
     return;
   }
   if(recv_buf_pos < RECV_BUF_SIZE - 1) {
@@ -228,13 +210,13 @@ void CHANNEL__RECV(void)
       goto get_buffered;
     }
     perror("recv");
-    channel_string_to_cobol("Hung up");
-    channel_set_status(EHUP);
+    channel_string_to_cobol(cobol_buffer, "Hung up");
+    channel_set_status(state, EHUP);
     return;
   }
 
-  channel_string_to_cobol("Server failed to send newline");
-  channel_set_status(ESERV);
+  channel_string_to_cobol(cobol_buffer, "Server failed to send newline");
+  channel_set_status(state, ESERV);
   return;
 }
 
